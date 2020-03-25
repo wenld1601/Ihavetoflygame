702c85d2850730328fb9fac19551d436a5201f44
Higgins Weng <wenld1601@gmail.com>
2020-03-25 14:47:50 -0700

    Initial commit

---
+++ app/src/main/java/com/example/ihavetoflygame/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ihavetoflygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+    }
+}
