bba26bb173d965c1489d8876cc39e21a7ddb35b0
Higgins Weng <wenld1601@gmail.com>
2020-03-26 22:16:40 -0700

    Add bullet and bird animation

--- app/src/main/java/com/example/ihavetoflygame/Flight.java
+++ app/src/main/java/com/example/ihavetoflygame/Flight.java
@@ -3,6 +3,7 @@ package com.example.ihavetoflygame;
 import android.graphics.Bitmap;
 import android.content.res.Resources;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 
 import static com.example.ihavetoflygame.GameView.screenRatioX;
 import static com.example.ihavetoflygame.GameView.screenRatioY;
@@ -10,11 +11,15 @@ import static com.example.ihavetoflygame.GameView.screenRatioY;
 
 public class Flight {
 
-    public boolean isGoingUp = false;
-    int x, y, width, height, wingCounter = 0;
-    Bitmap flight1, flight2;
+    int toShoot = 0;
+    boolean isGoingUp = false;
+    int x, y, width, height, wingCounter = 0, shootCounter = 1;
+    Bitmap flight1, flight2, shoot1, shoot2, shoot3, shoot4, shoot5, dead;
+    private GameView gameView;
 
-    Flight (int screenY, Resources res){
+    Flight (GameView gameView, int screenY, Resources res){
+
+        this.gameView = gameView;
 
         flight1 = BitmapFactory.decodeResource(res, R.drawable.fly1);
         flight2 = BitmapFactory.decodeResource(res, R.drawable.fly2);
@@ -31,12 +36,59 @@ public class Flight {
         flight1 = Bitmap.createScaledBitmap(flight1, width, height, false);
         flight2 = Bitmap.createScaledBitmap(flight2, width, height, false);
 
+        shoot1 = BitmapFactory.decodeResource(res, R.drawable.shoot1);
+        shoot2 = BitmapFactory.decodeResource(res, R.drawable.shoot2);
+        shoot3 = BitmapFactory.decodeResource(res, R.drawable.shoot3);
+        shoot4 = BitmapFactory.decodeResource(res, R.drawable.shoot4);
+        shoot5 = BitmapFactory.decodeResource(res, R.drawable.shoot5);
+
+        shoot1 = Bitmap.createScaledBitmap(shoot1, width, height,false);
+        shoot2 = Bitmap.createScaledBitmap(shoot2, width, height,false);
+        shoot3 = Bitmap.createScaledBitmap(shoot3, width, height,false);
+        shoot4 = Bitmap.createScaledBitmap(shoot4, width, height,false);
+        shoot5 = Bitmap.createScaledBitmap(shoot5, width, height,false);
+
+        dead = BitmapFactory.decodeResource(res, R.drawable.dead);
+        dead = Bitmap.createScaledBitmap(dead, width, height, false);
+
+
         y = screenY/2;
         x = (int)(64*screenRatioX);
     }
 
 
     Bitmap getFlight(){
+
+        if (toShoot != 0){
+
+            if (shootCounter == 1){
+                shootCounter++;
+                return shoot1;
+            }
+
+            if (shootCounter == 2){
+                shootCounter++;
+                return shoot2;
+            }
+
+            if (shootCounter == 3){
+                shootCounter++;
+                return shoot3;
+            }
+
+            if (shootCounter == 4){
+                shootCounter++;
+                return shoot4;
+            }
+
+            shootCounter = 1;
+            toShoot--;
+            gameView.newBullet();
+
+
+            return shoot5;
+
+        }
 
         if (wingCounter == 0){
             wingCounter++;
@@ -46,6 +98,14 @@ public class Flight {
 
         return flight2;
 
+    }
+
+    Rect getCollisionShape(){
+        return new Rect(x, y, x + width, y + height);
+    }
+
+    Bitmap getDead(){
+        return dead;
     }
 
 }
--- app/src/main/java/com/example/ihavetoflygame/GameView.java
+++ app/src/main/java/com/example/ihavetoflygame/GameView.java
@@ -3,16 +3,24 @@ package com.example.ihavetoflygame;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class GameView extends SurfaceView implements Runnable {
 
     private Thread thread;
-    private boolean isPlaying;
+    private boolean isPlaying, isGameOver = false;
     private int screenX, screenY;
     public static float screenRatioX, screenRatioY;
     private Paint paint;
+    private Bird[] birds;
+    private Random random;
+    private List<Bullet> bullets;
     private Flight flight;
     private Background background1, background2;
 
@@ -27,11 +35,24 @@ public class GameView extends SurfaceView implements Runnable {
         background1 = new Background(screenX, screenY, getResources());
         background2 = new Background(screenX, screenY, getResources());
 
-        flight = new Flight(screenY, getResources());
+        flight = new Flight(this, screenY, getResources());
+
+        bullets = new ArrayList<>();
 
         background2.x = screenX;
 
         paint = new Paint();
+
+        birds = new Bird[4];
+
+        for (int i = 0; i < 4; i++){
+
+            Bird bird = new Bird(getResources());
+            birds[i] = bird;
+        }
+
+        random = new Random();
+
     }
 
     @Override
@@ -66,6 +87,64 @@ public class GameView extends SurfaceView implements Runnable {
             flight.y = 0;
         if (flight.y > screenY - flight.height)
             flight.y = screenY - flight.height;
+
+        List<Bullet> trash = new ArrayList<>();
+
+        for (Bullet bullet: bullets){
+
+            if (bullet.x > screenX)
+                trash.add(bullet);
+
+            bullet.x += 50 * screenRatioX;
+
+            for(Bird bird : birds){
+                if(Rect.intersects(bird.getCollisionShape(), bullet.getCollisionShape())){
+
+                    bird.x = -500;
+                    bullet.x = screenX + 500;
+                    bird.wasShot = true;
+
+                }
+            }
+
+        }
+
+        for (Bullet bullet : trash)
+            bullets.remove(bullet);
+
+        for (Bird bird : birds){
+
+            bird.x -= bird.speed;
+
+            if (bird.x + bird.width < 0){
+
+                if(!bird.wasShot){
+                    isGameOver = true;
+                    return;
+                }
+
+                int bound = (int) (30 * screenRatioX);
+                bird.speed = random.nextInt(bound);
+
+                if (bird.speed < 10 * screenRatioX)
+                    bird.speed = (int) (10*screenRatioX);
+
+                bird.x = screenX;
+                bird.y = random.nextInt(screenY - bird.height);
+
+                bird.wasShot = false;
+            }
+
+            if (Rect.intersects(bird.getCollisionShape(), flight.getCollisionShape())){
+
+                isGameOver = true;
+                return;
+
+            }
+
+        }
+
+
     }
 
     private void draw(){
@@ -76,8 +155,20 @@ public class GameView extends SurfaceView implements Runnable {
             canvas.drawBitmap(background1.background, background1.x, background1.y, paint);
             canvas.drawBitmap(background2.background, background2.x, background2.y, paint);
 
+            if(isGameOver){
+                isPlaying = false;
+                canvas.drawBitmap(flight.getDead(), flight.x, flight.y, paint);
+                getHolder().unlockCanvasAndPost(canvas);
+                return;
+            }
+
+            for (Bird bird: birds)
+                canvas.drawBitmap(bird.getBird(), bird.x, bird.y, paint);
+
             canvas.drawBitmap(flight.getFlight(), flight.x, flight.y, paint);
 
+            for (Bullet bullet : bullets)
+                canvas.drawBitmap(bullet.bullet, bullet.x, bullet.y, paint);
 
             getHolder().unlockCanvasAndPost(canvas);
 
@@ -122,9 +213,22 @@ public class GameView extends SurfaceView implements Runnable {
                 break;
             case MotionEvent.ACTION_UP:
                 flight.isGoingUp = false;
+                if (event.getX() > screenX / 2)
+                    flight.toShoot++;
+
                 break;
         }
 
         return true;
+    }
+
+    public void newBullet() {
+
+        Bullet bullet = new Bullet(getResources());
+
+        bullet.x = flight.x + flight.width;
+        bullet.y = flight.y + (flight.height/2);
+        bullets.add(bullet);
+
     }
 }
